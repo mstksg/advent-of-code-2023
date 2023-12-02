9ae38de9db3796f71892a8328a7fc99ff59af825
justin <justin@jle.im>
2023-12-02 06:41:09 +0000

    clean up day 2

--- src/AOC/Challenge/Day02.hs
+++ src/AOC/Challenge/Day02.hs
@@ -1,6 +1,3 @@
-{-# OPTIONS_GHC -Wno-unused-imports   #-}
-{-# OPTIONS_GHC -Wno-unused-top-binds #-}
-
 -- |
 -- Module      : AOC.Challenge.Day02
 -- License     : BSD3
@@ -9,43 +6,22 @@
 -- Portability : non-portable
 --
 -- Day 2.  See "AOC.Solver" for the types used in this module!
---
--- After completing the challenge, it is recommended to:
---
--- *   Replace "AOC.Prelude" imports to specific modules (with explicit
---     imports) for readability.
--- *   Remove the @-Wno-unused-imports@ and @-Wno-unused-top-binds@
---     pragmas.
--- *   Replace the partial type signatures underscores in the solution
---     types @_ :~> _@ with the actual types of inputs and outputs of the
---     solution.  You can delete the type signatures completely and GHC
---     will recommend what should go in place of the underscores.
 
 module AOC.Challenge.Day02 (
     day02a
   , day02b
   ) where
 
-import           AOC.Prelude
-
-import qualified Data.Graph.Inductive           as G
-import qualified Data.IntMap                    as IM
-import qualified Data.IntSet                    as IS
-import qualified Data.List.NonEmpty             as NE
-import qualified Data.List.PointedList          as PL
-import qualified Data.List.PointedList.Circular as PLC
-import qualified Data.Map                       as M
-import qualified Data.OrdPSQ                    as PSQ
-import qualified Data.Sequence                  as Seq
-import qualified Data.Set                       as S
-import qualified Data.Text                      as T
-import qualified Data.Vector                    as V
-import qualified Linear                         as L
-import qualified Text.Megaparsec                as P
-import qualified Text.Megaparsec.Char           as P
-import qualified Text.Megaparsec.Char.Lexer     as PP
-
--- Game 1: 14 green, 8 blue, 9 red; 5 blue, 4 green, 2 red; 4 red, 4 blue, 4 green; 1 blue, 3 green, 2 red; 10 red, 3 blue, 15 green; 2 red, 6 green, 3 blue
+import AOC.Common (listTup)
+import AOC.Solver ((:~>)(..))
+import Control.Applicative (liftA2)
+import Control.Monad (guard)
+import Data.Functor ((<&>))
+import Data.List.Split (splitOn)
+import Data.Maybe (mapMaybe, fromMaybe)
+import Data.Traversable (for)
+import Linear.V3 (V3(..))
+import Text.Read (readMaybe)
 
 parseLine :: String -> Maybe (Int, [V3 Int])
 parseLine fullLine = do
@@ -59,7 +35,7 @@ parseLine fullLine = do
           fromMaybe 0 $ readMaybe =<< lookup col gs
     pure (i, sets)
 
-day02a :: _ :~> _
+day02a :: [(Int, [V3 Int])] :~> Int
 day02a = MkSol
     { sParse = traverse parseLine . lines
     , sShow  = show
@@ -68,11 +44,11 @@ day02a = MkSol
   where
     isLegal = all (and . liftA2 (>=) (V3 12 13 14))
 
-day02b :: _ :~> _
+day02b :: [[V3 Int]] :~> Int
 day02b = MkSol
-    { sParse = sParse day02a
+    { sParse = fmap (map snd) . traverse parseLine . lines
     , sShow  = show
-    , sSolve = Just . sum . map (calcPower . snd)
+    , sSolve = Just . sum . map calcPower
     }
   where
     calcPower = product . foldr (liftA2 max) 0
