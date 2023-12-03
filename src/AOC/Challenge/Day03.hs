670d801d3dd529e0caa0b14f3b29fe144fc5ed65
justin <justin@jle.im>
2023-12-03 16:42:59 +0000

    clean up day 3

--- src/AOC/Challenge/Day03.hs
+++ src/AOC/Challenge/Day03.hs
@@ -1,6 +1,3 @@
-{-# OPTIONS_GHC -Wno-unused-imports   #-}
-{-# OPTIONS_GHC -Wno-unused-top-binds #-}
-
 -- |
 -- Module      : AOC.Challenge.Day03
 -- License     : BSD3
@@ -26,52 +23,45 @@ module AOC.Challenge.Day03 (
   , day03b
   ) where
 
-import           AOC.Prelude
-
-import qualified Data.Graph.Inductive           as G
-import qualified Data.IntMap                    as IM
-import qualified Data.IntSet                    as IS
-import qualified Data.List.NonEmpty             as NE
-import qualified Data.List.PointedList          as PL
-import qualified Data.List.PointedList.Circular as PLC
+import AOC.Common (listTup)
+import AOC.Common.Point (Point, fullNeighbs, contiguousRegions, fullNeighbsSet, parseAsciiMap)
+import AOC.Solver ((:~>)(..))
+import Data.Bifunctor (first)
+import Data.Char (isDigit)
+import Data.Map (Map)
+import Data.Maybe (mapMaybe)
+import Data.Set (Set)
+import Text.Read (readMaybe)
 import qualified Data.Map                       as M
-import qualified Data.OrdPSQ                    as PSQ
-import qualified Data.Sequence                  as Seq
 import qualified Data.Set                       as S
 import qualified Data.Set.NonEmpty              as NES
-import qualified Data.Text                      as T
-import qualified Data.Vector                    as V
-import qualified Linear                         as L
-import qualified Text.Megaparsec                as P
-import qualified Text.Megaparsec.Char           as P
-import qualified Text.Megaparsec.Char.Lexer     as PP
 
-day03a :: _ :~> _
+day03a :: (Set Point, Map Point Char) :~> Int
 day03a = MkSol
     { sParse = Just . first M.keysSet . M.mapEither id . parseAsciiMap (\case
           '.' -> Nothing
-          c   -> Just $ maybeToEither c (digitToIntSafe c)
+          c | isDigit c -> Just $ Right c
+            | otherwise -> Just $ Left ()
         )
     , sShow  = show
     , sSolve = \(symbolPoints, numPoints) ->
         let numChunks = contiguousRegions (M.keysSet numPoints)
-            adjacentToSymbols = S.fromList $
-              fullNeighbs =<< S.toList symbolPoints
+            adjacentToSymbols = fullNeighbsSet `foldMap` S.toList symbolPoints
             validNumChunks = flip S.filter numChunks \numChunk ->
               not $ NES.toSet numChunk `S.disjoint` adjacentToSymbols
-            numStrings = map intToDigit . M.elems
+            numStrings = M.elems
                   . M.restrictKeys numPoints
                   . NES.toSet
                 <$> S.toList validNumChunks
          in sum <$> traverse (readMaybe @Int) numStrings
     }
 
-day03b :: _ :~> _
+day03b :: (Set Point, Map Point Char) :~> Int
 day03b = MkSol
     { sParse = Just . first M.keysSet . M.mapEither id . parseAsciiMap (\case
-          '.' -> Nothing
           '*' -> Just $ Left ()
-          c   -> Right <$> digitToIntSafe c
+          c | isDigit c -> Just $ Right c
+          _ -> Nothing
         )
     , sShow  = show
     , sSolve = \(symbolPoints, numPoints) ->
@@ -87,6 +77,6 @@ day03b = MkSol
                 adjacentToSymbols `M.restrictKeys` rg
               pure (orig, S.singleton rgDigits)
          in fmap (sum . map (uncurry (*)) . mapMaybe listTup)
-              . traverse (traverse (readMaybe @Int . map intToDigit))
+              . traverse (traverse (readMaybe @Int))
               $ S.toList <$> M.elems validNumChunks
     }
