c8e8746da36184566640c7e6b1d27ada93e24670
justin <justin@jle.im>
2023-12-04 06:04:10 +0000

    code formatting

--- src/AOC/Challenge/Day03.hs
+++ src/AOC/Challenge/Day03.hs
@@ -6,55 +6,69 @@
 -- Portability : non-portable
 --
 -- Day 3.  See "AOC.Solver" for the types used in this module!
-
-module AOC.Challenge.Day03 (
-    day03a
-  , day03b
-  ) where
+module AOC.Challenge.Day03
+  ( day03a,
+    day03b,
+  )
+where
 
 import AOC.Common (listTup)
-import AOC.Common.Point (Point, fullNeighbs, contiguousRegions, fullNeighbsSet, parseAsciiMap)
-import AOC.Solver ((:~>)(..))
+import AOC.Common.Point (Point, contiguousRegions, fullNeighbs, fullNeighbsSet, parseAsciiMap)
+import AOC.Solver ((:~>) (..))
+import Control.Lens (each, traverseOf)
 import Control.Monad (guard)
 import Data.Bifunctor (first)
-import Control.Lens (each, traverseOf)
 import Data.Char (isDigit)
 import Data.Map (Map)
+import qualified Data.Map as M
 import Data.Maybe (mapMaybe)
 import Data.Set (Set)
+import qualified Data.Set as S
+import qualified Data.Set.NonEmpty as NES
 import Text.Read (readMaybe)
-import qualified Data.Map                       as M
-import qualified Data.Set                       as S
-import qualified Data.Set.NonEmpty              as NES
 
 day03a :: (Set Point, Map Point Char) :~> Int
-day03a = MkSol
-    { sParse = Just . first M.keysSet . M.mapEither id . parseAsciiMap (\case
-          '.' -> Nothing
-          c | isDigit c -> Just $ Right c
-            | otherwise -> Just $ Left ()
-        )
-    , sShow  = show
-    , sSolve = \(symbolPoints, numPoints) ->
+day03a =
+  MkSol
+    { sParse =
+        Just
+          . first M.keysSet
+          . M.mapEither id
+          . parseAsciiMap
+            ( \case
+                '.' -> Nothing
+                c
+                  | isDigit c -> Just $ Right c
+                  | otherwise -> Just $ Left ()
+            ),
+      sShow = show,
+      sSolve = \(symbolPoints, numPoints) ->
         let numChunks = contiguousRegions (M.keysSet numPoints)
             adjacentToSymbols = fullNeighbsSet `foldMap` S.toList symbolPoints
             validNumChunks = flip S.filter numChunks \numChunk ->
               not $ NES.toSet numChunk `S.disjoint` adjacentToSymbols
-            numStrings = M.elems
-                  . M.restrictKeys numPoints
-                  . NES.toSet
+            numStrings =
+              M.elems
+                . M.restrictKeys numPoints
+                . NES.toSet
                 <$> S.toList validNumChunks
          in sum <$> traverse readMaybe numStrings
     }
 
 day03b :: (Set Point, Map Point Char) :~> Int
-day03b = MkSol
-    { sParse = Just . first M.keysSet . M.mapEither id . parseAsciiMap (\case
-          '*' -> Just $ Left ()
-          c   -> Right c <$ guard (isDigit c)
-        )
-    , sShow  = show
-    , sSolve = \(symbolPoints, numPoints) ->
+day03b =
+  MkSol
+    { sParse =
+        Just
+          . first M.keysSet
+          . M.mapEither id
+          . parseAsciiMap
+            ( \case
+                '*' -> Just $ Left ()
+                c -> Right c <$ guard (isDigit c)
+            ),
+      sShow = show,
+      sSolve = \(symbolPoints, numPoints) ->
         let numChunks = contiguousRegions (M.keysSet numPoints)
             adjacentToSymbols = M.fromList do
               p <- S.toList symbolPoints
@@ -63,8 +77,9 @@ day03b = MkSol
             validNumChunks = M.fromListWith (<>) do
               rg <- NES.toSet <$> S.toList numChunks
               let rgDigits = M.elems $ M.restrictKeys numPoints rg
-              orig <- M.elems $
-                adjacentToSymbols `M.restrictKeys` rg
+              orig <-
+                M.elems $
+                  adjacentToSymbols `M.restrictKeys` rg
               pure (orig, S.singleton rgDigits)
          in fmap (sum . map (uncurry (*)))
               . traverse (traverseOf each readMaybe)
--- src/AOC/Challenge/Day04.hs
+++ src/AOC/Challenge/Day04.hs
@@ -6,52 +6,55 @@
 -- Portability : non-portable
 --
 -- Day 4.  See "AOC.Solver" for the types used in this module!
-
-module AOC.Challenge.Day04 (
-    day04a
-  , day04b
-  ) where
+module AOC.Challenge.Day04
+  ( day04a,
+    day04b,
+  )
+where
 
 import AOC.Common (listTup)
-import AOC.Solver (noFail, (:~>)(..))
+import AOC.Solver (noFail, (:~>) (..))
 import Control.Monad ((<=<))
-import Control.Monad.State (state, evalState)
+import Control.Monad.State (evalState, state)
 import Data.List.Split (splitOn)
+import qualified Data.Map as M
 import Data.Set (Set)
+import qualified Data.Set as S
 import Data.Traversable (for)
 import Safe (tailMay)
 import Text.Read (readMaybe)
-import qualified Data.Map as M
-import qualified Data.Set as S
 
 parseCard :: String -> Maybe (Set Int, Set Int)
 parseCard =
-      listTup
-  <=< traverse (fmap S.fromList . traverse readMaybe . words) . splitOn "|"
-  <=< tailMay . dropWhile (/= ':')
+  listTup
+    <=< traverse (fmap S.fromList . traverse readMaybe . words) . splitOn "|"
+    <=< tailMay . dropWhile (/= ':')
 
 cardWins :: (Set Int, Set Int) -> Int
 cardWins = S.size . uncurry S.intersection
 
 day04a :: [(Set Int, Set Int)] :~> Int
-day04a = MkSol
-    { sParse = traverse parseCard . lines
-    , sShow  = show
-    , sSolve = noFail $
+day04a =
+  MkSol
+    { sParse = traverse parseCard . lines,
+      sShow = show,
+      sSolve =
+        noFail $
           sum . map ((2 ^) . subtract 1) . filter (> 0) . map cardWins
     }
 
 day04b :: [(Set Int, Set Int)] :~> Int
-day04b = MkSol
-    { sParse = traverse parseCard . lines
-    , sShow  = show
-    , sSolve = noFail $ \cards ->
-        let ixedCards = M.fromList $ zip [1..] cards
+day04b =
+  MkSol
+    { sParse = traverse parseCard . lines,
+      sShow = show,
+      sSolve = noFail \cards ->
+        let ixedCards = M.fromList $ zip [1 ..] cards
          in sum . flip evalState (1 <$ ixedCards) $
-              for (M.toList ixedCards) \(i, (a,b)) -> state \currState ->
+              for (M.toList ixedCards) \(i, (a, b)) -> state \currState ->
                 let amount = currState M.! i
                     rest = M.delete i currState
                     n = S.size $ a `S.intersection` b
-                    newCards = M.fromList ((,amount) . (+i) <$> [1 .. n])
+                    newCards = M.fromList ((,amount) . (+ i) <$> [1 .. n])
                  in (currState M.! i, M.unionWith (+) newCards rest)
     }
