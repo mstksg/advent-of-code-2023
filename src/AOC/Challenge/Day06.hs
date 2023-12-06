1edd95d30a46ecf78ecf2e19ab15549bd972edf5
justin <justin@jle.im>
2023-12-06 06:12:45 +0000

    clean up day 6

--- src/AOC/Challenge/Day06.hs
+++ src/AOC/Challenge/Day06.hs
@@ -1,6 +1,3 @@
-{-# OPTIONS_GHC -Wno-unused-imports #-}
-{-# OPTIONS_GHC -Wno-unused-top-binds #-}
-
 -- |
 -- Module      : AOC.Challenge.Day06
 -- License     : BSD3
@@ -9,93 +6,50 @@
 -- Portability : non-portable
 --
 -- Day 6.  See "AOC.Solver" for the types used in this module!
---
--- After completing the challenge, it is recommended to:
---
--- *   Replace "AOC.Prelude" imports to specific modules (with explicit
---     imports) for readability.
--- *   Remove the @-Wno-unused-imports@ and @-Wno-unused-top-binds@
---     pragmas.
--- *   Replace the partial type signatures underscores in the solution
---     types @_ :~> _@ with the actual types of inputs and outputs of the
---     solution.  You can delete the type signatures completely and GHC
---     will recommend what should go in place of the underscores.
 module AOC.Challenge.Day06
   ( day06a,
     day06b,
   )
 where
 
-import AOC.Prelude
-import qualified Data.Graph.Inductive as G
-import qualified Data.IntMap as IM
-import qualified Data.IntSet as IS
-import qualified Data.List.NonEmpty as NE
-import qualified Data.List.PointedList as PL
-import qualified Data.List.PointedList.Circular as PLC
-import qualified Data.Map as M
-import qualified Data.OrdPSQ as PSQ
-import qualified Data.Sequence as Seq
-import qualified Data.Set as S
-import qualified Data.Text as T
-import qualified Data.Vector as V
-import qualified Linear as L
-import qualified Text.Megaparsec as P
-import qualified Text.Megaparsec.Char as P
-import qualified Text.Megaparsec.Char.Lexer as PP
-
--- Time:      7  15   30
--- Distance:  9  40  200
+import AOC.Common (listTup)
+import AOC.Solver (noFail, (:~>) (..))
+import Control.Monad ((<=<))
+import Data.Char (isDigit)
+import Data.List (transpose)
+import Text.Read (readMaybe)
 
 getWays :: Int -> Int -> Int
-getWays tm dm =
-  length
-    [ ()
-      | t <- [0 .. tm],
-        let vf = t
-            restT = tm - vf
-            d = restT * vf,
-        d > dm
-    ]
+getWays tMax dRecord = isqrt (tMax * tMax - 4 * dRecord - 2)
 
-getWays' :: Double -> Double -> Int
-getWays' tm dm = floor $
-  0.5 * (tm + sqrt(tm^2 - 4*dm)) - 0.5 * (tm - sqrt(tm^2 - 4*dm))
--- 1/2 (m - sqrt(m^2 - 4 d))<t<1/2 (sqrt(m^2 - 4 d) + m) and d<m^2/4 and m element R
-  -- length
-  --   [ ()
-  --     | t <- [0 .. tm],
-  --       let vf = t
-  --           restT = tm - vf
-  --           d = restT * vf,
-  --       d > dm
-  --   ]
-
-
--- (tm - t) * t = tm * t - t ^2 > dm
---
--- tm * t - t^2 > dm
---
+-- | integer square root
+isqrt :: Int -> Int
+isqrt n
+  | n < 2 = n
+  | otherwise = go n
+  where
+    go !x
+      | x == y = x
+      | otherwise = go y
+      where
+        y = (x + n `div` x) `div` 2
 
 day06a :: _ :~> _
 day06a =
   MkSol
     { sParse =
-        noFail $
-          map (fromJust . listTup) . transpose . map (map (read @Int) . tail . words) . lines,
+        traverse listTup
+          . transpose
+          <=< traverse (traverse readMaybe . drop 1 . words)
+            . lines,
       sShow = show,
-      sSolve =
-        noFail $
-          product . map (uncurry getWays)
+      sSolve = noFail $ product . map (uncurry getWays)
     }
 
 day06b :: _ :~> _
 day06b =
   MkSol
-    { sParse = noFail $
-          fromJust . listTup . map (read @Int . filter isDigit) . lines,
+    { sParse = listTup <=< traverse (readMaybe . filter isDigit) . lines,
       sShow = show,
-      sSolve =
-        noFail $
-          \(x, y) -> getWays' (fromIntegral x) (fromIntegral y)
+      sSolve = noFail $ uncurry getWays
     }
