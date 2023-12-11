9f9d98046abb26ac431d7bf511c5a822694a39d9
justin <justin@jle.im>
2023-12-11 05:29:24 +0000

    clean up day 11

--- src/AOC/Challenge/Day11.hs
+++ src/AOC/Challenge/Day11.hs
@@ -1,100 +1,59 @@
-{-# OPTIONS_GHC -Wno-unused-imports #-}
-{-# OPTIONS_GHC -Wno-unused-top-binds #-}
-
 -- |
 -- Module      : AOC.Challenge.Day11
 -- License     : BSD3
 --
 -- Stability   : experimental
 -- Portability : non-portable
---
--- Day 11.  See "AOC.Solver" for the types used in this module!
---
--- After completing the challenge, it is recommended to:
---
--- *   Replace "AOC.Prelude" imports to specific modules (with explicit
---     imports) for readability.
--- *   Remove the @-Wno-unused-imports@ and @-Wno-unused-top-binds@
---     pragmas.
--- *   Replace the partial type signatures underscores in the solution
---     types @_ :~> _@ with the actual types of inputs and outputs of the
---     solution.  You can delete the type signatures completely and GHC
---     will recommend what should go in place of the underscores.
 module AOC.Challenge.Day11
   ( day11a,
     day11b,
   )
 where
 
-import AOC.Prelude
-import qualified Data.Graph.Inductive as G
-import qualified Data.IntMap as IM
+import AOC.Solver ((:~>)(..), noFail)
+import AOC.Common.Point (Point, parseAsciiSet, mannDist, boundingBox)
+import Data.Set.NonEmpty (NESet)
+import Data.IntSet (IntSet)
+import Linear.V2 (V2(..))
+import Data.Foldable (toList)
+import Data.List (tails)
 import qualified Data.IntSet as IS
-import qualified Data.List.NonEmpty as NE
-import qualified Data.List.PointedList as PL
-import qualified Data.List.PointedList.Circular as PLC
-import qualified Data.Map as M
-import qualified Data.OrdPSQ as PSQ
-import qualified Data.Sequence as Seq
-import qualified Data.Set as S
-import qualified Data.Text as T
-import qualified Data.Vector as V
-import qualified Linear as L
-import qualified Text.Megaparsec as P
-import qualified Text.Megaparsec.Char as P
-import qualified Text.Megaparsec.Char.Lexer as PP
+import qualified Data.Set.NonEmpty as NES
 
-expand :: Set Point -> Set Point
-expand orig = S.map reshape orig
+expandBy :: Int -> NESet Point -> NESet Point
+expandBy toAdd orig = NES.mapMonotonic reshape orig
   where
-    reshape (V2 x y) = V2 (x + underX) (y + underY)
+    reshape (V2 x y) = V2 (x + underX * toAdd) (y + underY * toAdd)
       where
-        underX = length $ takeWhile (< x) blankXs
-        underY = length $ takeWhile (< y) blankYs
-    Just (V2 (V2 xMin yMin) (V2 xMax yMax)) = boundingBox' orig
-    blankXs = flip filter [xMin .. xMax] \x -> S.null $ orig `S.intersection` (S.fromList $ V2 x <$> [yMin .. yMax])
-    blankYs = flip filter [yMin .. yMax] \y -> S.null $ orig `S.intersection` (S.fromList $ flip V2 y <$> [xMin .. xMax])
+        underX = IS.size $ IS.takeWhileAntitone (< x) blankXs
+        underY = IS.size $ IS.takeWhileAntitone (< y) blankYs
+    V2 (V2 xMin yMin) (V2 xMax yMax) = boundingBox orig
+    blankXs :: IntSet
+    blankXs = IS.fromDistinctAscList $
+      flip filter [xMin .. xMax] \x ->
+        all ((`NES.notMember` orig) . V2 x) [yMin .. yMax]
+    blankYs :: IntSet
+    blankYs = IS.fromDistinctAscList $
+      flip filter [yMin .. yMax] \y ->
+        all ((`NES.notMember` orig) . flip V2 y) [yMin .. yMax]
 
-expand2 :: Set Point -> Set Point
-expand2 orig = S.map reshape orig
-  where
-    reshape (V2 x y) = V2 (x + underX * (1000000 - 1)) (y + underY * (1000000 - 1))
-      where
-        underX = length $ takeWhile (< x) blankXs
-        underY = length $ takeWhile (< y) blankYs
-    Just (V2 (V2 xMin yMin) (V2 xMax yMax)) = boundingBox' orig
-    blankXs = flip filter [xMin .. xMax] \x -> S.null $ orig `S.intersection` (S.fromList $ V2 x <$> [yMin .. yMax])
-    blankYs = flip filter [yMin .. yMax] \y -> S.null $ orig `S.intersection` (S.fromList $ flip V2 y <$> [xMin .. xMax])
-
-day11a :: _ :~> _
-day11a =
+day11 :: Int -> NESet Point :~> Int
+day11 toAdd =
   MkSol
-    { sParse = noFail $ parseAsciiSet (== '#'),
-      -- , sShow  = ('\n':) . displayAsciiSet '.' '#'
+    { sParse = NES.nonEmptySet . parseAsciiSet (== '#'),
       sShow = show,
       sSolve = noFail $
         \xs ->
-          let xs' = expand xs
+          let xs' = expandBy toAdd xs
            in sum
                 [ mannDist x y
-                  | (x :: Point, ys) <- select (toList xs'),
-                    y :: Point <- ys
+                  | x : ys <- tails $ toList xs',
+                    y <- ys
                 ]
-                `div` 2
     }
 
-day11b :: _ :~> _
-day11b =
-  MkSol
-    { sParse = sParse day11a,
-      sShow = show,
-      sSolve = noFail $
-        \xs ->
-          let xs' = expand2 xs
-           in sum
-                [ mannDist x y
-                  | (x :: Point, ys) <- select (toList xs'),
-                    y :: Point <- ys
-                ]
-                `div` 2
-    }
+day11a :: NESet Point :~> Int
+day11a = day11 1
+
+day11b :: NESet Point :~> Int
+day11b = day11 999999
