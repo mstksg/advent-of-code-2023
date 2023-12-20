31dbbec7115797e9d18cdbc9e446ab125a0eda41
justin <justin@jle.im>
2023-12-20 09:03:51 +0000

    more progress

--- src/AOC/Challenge/Day20.hs
+++ src/AOC/Challenge/Day20.hs
@@ -1,4 +1,4 @@
-{-# OPTIONS_GHC -Wno-unused-imports   #-}
+{-# OPTIONS_GHC -Wno-unused-imports #-}
 {-# OPTIONS_GHC -Wno-unused-top-binds #-}
 
 -- |
@@ -20,31 +20,31 @@
 --     types @_ :~> _@ with the actual types of inputs and outputs of the
 --     solution.  You can delete the type signatures completely and GHC
 --     will recommend what should go in place of the underscores.
+module AOC.Challenge.Day20
+  ( day20a,
+    day20b,
+  )
+where
 
-module AOC.Challenge.Day20 (
-    day20a
-  , day20b
-  ) where
-
-import           AOC.Prelude
-
-import qualified Data.Graph.Inductive           as G
-import qualified Data.IntMap                    as IM
-import qualified Data.IntSet                    as IS
-import qualified Data.List.NonEmpty             as NE
-import qualified Data.List.PointedList          as PL
-import qualified Data.List.PointedList.Circular as PLC
-import qualified Data.Map                       as M
-import qualified Data.OrdPSQ                    as PSQ
-import qualified Data.Sequence                  as Seq
-import qualified Data.Set                       as S
-import qualified Data.Text                      as T
-import qualified Data.Vector                    as V
-import qualified Linear                         as L
+import AOC.Prelude
 import Data.Bitraversable
-import qualified Text.Megaparsec                as P
-import qualified Text.Megaparsec.Char           as P
-import qualified Text.Megaparsec.Char.Lexer     as PP
+import qualified Data.Graph.Inductive as G
+import qualified Data.IntMap as IM
+import qualified Data.IntSet as IS
+import qualified Data.List.NonEmpty as NE
+import qualified Data.List.PointedList as PL
+import qualified Data.List.PointedList.Circular as PLC
+import qualified Data.Map as M
+import qualified Data.OrdPSQ as PSQ
+import qualified Data.Sequence as Seq
+import qualified Data.Set as S
+import qualified Data.Text as T
+import Data.Tuple.Strict (T2 (..))
+import qualified Data.Vector as V
+import qualified Linear as L
+import qualified Text.Megaparsec as P
+import qualified Text.Megaparsec.Char as P
+import qualified Text.Megaparsec.Char.Lexer as PP
 
 data ModuleType = FlipFlop | Conjuction
   deriving stock (Show, Eq, Ord, Generic)
@@ -55,41 +55,75 @@ parseLine :: String -> Maybe (Maybe (ModuleType, String), [String])
 parseLine = bitraverse pName (pure . pDest) <=< listTup . splitOn " -> "
   where
     pName = \case
-      '%':xs -> Just . Just $ (FlipFlop, xs)
-      '&':xs -> Just . Just $ (Conjuction, xs)
+      '%' : xs -> Just . Just $ (FlipFlop, xs)
+      '&' : xs -> Just . Just $ (Conjuction, xs)
       "broadcaster" -> Just Nothing
       _ -> Nothing
     pDest = splitOn ", "
 
-data ModuleState = MS
-    { msFlipFlops :: !(Set String)
-    , msConjunctions :: !(Map String (Set String))
-    }
+data ModuleData = MD
+  { mdForward :: Set String,
+    mdBackward :: Set String,
+    mdType :: ModuleType
+  }
   deriving stock (Show, Generic)
+
+instance NFData ModuleData
+
+data ModuleState = MS
+  { msFlipFlops :: !(Set String),
+    msConjunctions :: !(Map String (Set String))
+  }
+  deriving stock (Show, Generic)
+
+instance NFData ModuleState
 
 data ModuleConfig = MC
-    { mcBroadcast :: Set String
-    , mcForward :: Map String (Set String)
-    , mcBackward :: Map String (Set String)
-    }
+  { mcBroadcast :: Set String,
+    mcModules :: Map String ModuleData
+  }
   deriving stock (Show, Generic)
 
--- assembleConfig :: [(Maybe (ModuleType, String), [String])] -> ModuleConfig
--- assembleConfig 
+instance NFData ModuleConfig
+
+assembleConfig :: [(Maybe (ModuleType, String), [String])] -> Maybe ModuleConfig
+assembleConfig ms = do
+  First bc <- broadcast
+  pure $
+    MC
+      { mcBroadcast = bc,
+        mcModules = M.intersectionWith (\(f,t) b -> MD f b t) forwards bckwrds
+      }
+  where
+    T2 broadcast forwards =
+      flip foldMap ms $ \(mn, ds) ->
+        case mn of
+          Just (a, n) -> T2 mempty (M.singleton n (S.fromList ds, a))
+          Nothing -> T2 (Just (First (S.fromList ds))) mempty
+    bckwrds =
+      M.fromListWith
+        (<>)
+        [ (v, S.singleton k)
+          | (k, (vs, _)) <- M.toList forwards,
+            v <- S.toList vs
+        ]
 
 day20a :: _ :~> _
-day20a = MkSol
-    { sParse = noFail $
-          traverse parseLine . lines
-    , sShow  = show
-    , sSolve = noFail $
-          id
+day20a =
+  MkSol
+    { sParse = traverse parseLine . lines,
+      sShow = show,
+      sSolve = assembleConfig
+        -- noFail $
+        --   id
     }
 
 day20b :: _ :~> _
-day20b = MkSol
-    { sParse = sParse day20a
-    , sShow  = show
-    , sSolve = noFail $
+day20b =
+  MkSol
+    { sParse = sParse day20a,
+      sShow = show,
+      sSolve =
+        noFail $
           id
     }
