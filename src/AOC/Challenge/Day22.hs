ccc3440e11b063e42b0126810769849e1059c163
justin <justin@jle.im>
2023-12-22 05:20:34 +0000

    day22 progress

--- src/AOC/Challenge/Day22.hs
+++ src/AOC/Challenge/Day22.hs
@@ -22,8 +22,8 @@
 --     will recommend what should go in place of the underscores.
 
 module AOC.Challenge.Day22 (
-    -- day22a
-  -- , day22b
+    day22a
+  , day22b
   ) where
 
 import           AOC.Prelude
@@ -45,16 +45,37 @@ import qualified Text.Megaparsec                as P
 import qualified Text.Megaparsec.Char           as P
 import qualified Text.Megaparsec.Char.Lexer     as PP
 
-day22a :: _ :~> _
+type Point3 = V3 Int
+
+dropBrick :: Seq (Set (V3 Int)) -> Seq (Set (V3 Int))
+dropBrick Seq.Empty = Seq.Empty
+dropBrick (x Seq.:<| xs) = xs Seq.:|> x'
+  where
+    taken = fold xs
+    dropped = S.map (over _z (subtract 1)) x
+    x' | S.null (dropped `S.intersection` taken) && all ((> 0) . view _z) dropped = dropped
+       | otherwise = x
+
+dropAllOnce :: Seq (Set (V3 Int)) -> Seq (Set (V3 Int))
+dropAllOnce xs = strictIterate dropBrick xs !! Seq.length xs
+
+-- firstRepeated :: Ord a => [a] -> Maybe a
+-- firstRepeated = firstRepeatedBy id
+
+day22a :: [(V2 (V3 Int))] :~> _
 day22a = MkSol
-    { sParse = Just . lines
+    { sParse = 
+          traverse (traverse (traverse readMaybe <=< listV3 . splitOn ",") <=< listV2 . splitOn "~") .lines
     , sShow  = show
-    , sSolve = Just
+    , sSolve = \xs ->
+      let bricks = Seq.fromList $ flip map xs \(V2 a b) -> S.fromList $ sequence $ liftA2 enumFromTo a b
+       in firstRepeated $ strictIterate dropAllOnce bricks
     }
 
 day22b :: _ :~> _
 day22b = MkSol
     { sParse = sParse day22a
     , sShow  = show
-    , sSolve = Just
+    , sSolve = noFail $
+          id
     }
